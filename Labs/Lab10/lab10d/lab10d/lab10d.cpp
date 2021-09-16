cc8d00b51304a3bca732c2e19f202a1520228c9c
Balugyan <90400920+Balugyan@users.noreply.github.com>
2021-09-16 17:13:44 +0300

    Initial commit

---
+++ Labs/Lab10/lab10d/lab10d/lab10d.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+#define N 3
+class train {
+public:
+    char m_name[100];
+    int m_number, m_time;
+    void Print() {
+        cout << "Train number: " << m_number << endl;
+        cout << "Destination name: " << m_name << endl;
+        cout << "Departure time: " << m_time << endl;
+        cout << endl;
+    }
+    void Input() {
+        cout << "Input a train number: ";
+        cin >> m_number;
+        cout << "Input a destination name: ";
+        cin >> m_name;
+        cout << "Input a departure time(h): ";
+        cin >> m_time;
+        cout << endl;
+    }
+};
+int main()
+{
+    train trains[N];
+    for (int i = 0; i < N; i++) {
+        trains[i].Input();
+    }
+    cout << endl;
+    //сортировка по номерам поездов
+    cout << "Sorting by train numbers\n1. Descending sorting\n2. Ascending sorting\n";
+    int num;
+    cout << "Input a number: ";
+    cin >> num;
+    cout << endl;
+    if (num == 1) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (trains[i].m_number > trains[j].m_number) {
+                    swap(trains[i], trains[j]);
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            trains[i].Print();
+        }
+    }
+    else if (num == 2) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (trains[i].m_number < trains[j].m_number) {
+                    swap(trains[i], trains[j]);
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            trains[i].Print();
+        }
+    }
+    else {
+        cout << "Invalid input.";
+    }
+    cout << endl;
+    //поиск;
+    int train_number;
+    cout << "Train search\nInput a train number: ";
+    cin >> train_number;
+    cout << endl;
+    for (int i = 0; i < N; i++) {
+        if (trains[i].m_number == train_number) {
+            trains[i].Print();
+        }
+    }
+    cout << endl;
+    //сортировка по пункту назначения
+    cout << "Sorting by destination:\n\n";
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - 1 - i; j++) {
+            int res = strcmp(trains[j].m_name, trains[j + 1].m_name);
+            if (res == 1)
+                swap(trains[j], trains[j + 1]);
+            else if (res == 0) {
+                if (trains[j].m_time > trains[j + 1].m_time)
+                    swap(trains[j], trains[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        trains[i].Print();
+    }
+    return 0;
+}
---
+++ Labs/Lab10/lab10e/lab10e/lab10e.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+class Matrix
+{
+private:
+    int n, m;
+    int** arr;
+public:
+    Matrix() {
+        n = m = 1;
+        arr = new int* [n];
+        for (int i = 0; i < 1; i++) {
+            arr[i] = new int[m];
+        };
+    }
+    Matrix(int n_size, int m_size) {
+        n = n_size;
+        m = m_size;
+        arr = new int* [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[m];
+        };
+    }
+    ~Matrix() {
+        for (int i = 0; i < n; i++)
+            delete arr[i];
+        delete[]arr;
+    }
+    void Rand() {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr[i][j] = rand() % 100;
+    }
+    void Print()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout << setw(3) << arr[i][j];
+            }
+            std::cout << endl;
+        }
+    }
+    void Min() {
+        int min;
+        int min_index_1, min_index_2;
+        min = arr[0][0];
+        for (int i = 0; i < n; i++)
+            for (int j = 1; j < m; j++)
+                if (min > arr[i][j])
+                {
+                    min = arr[i][j];
+                    min_index_1 = i;
+                    min_index_2 = j;
+                }
+        cout << "\nMin element = " << min << endl;
+    }
+    void Find() {
+        int index_1, index_2, num;
+        cout << "\nEnter the first index of the element you want to find: ";
+        cin >> index_1;
+        cout << "Enter the second index of the element you want to find: ";
+        cin >> index_2;
+        cout << "Element = " << arr[index_1][index_2] << endl;
+        cout << "Enter a number: ";
+        cin >> num;
+        arr[index_1][index_2] = num;
+        cout << "Element = " << arr[index_1][index_2] << endl;
+    }
+    void Submatrix() {
+        int num_1, num_2, a, b;
+        cout << "\nSubmatrix position:\nEnter the first number: ";
+        cin >> num_1;
+        cout << "Enter the second number: ";
+        cin >> num_2;
+        cout << "\nSubmatrix size:\nEnter the first number: ";
+        cin >> a;
+        cout << "Enter the second number: ";
+        cin >> b;
+        for (int i = num_1; i < num_1 + a; i++)
+        {
+            for (int j = num_2; j < num_2 + b; j++)
+            {
+                cout << setw(3) << arr[i][j];
+            }
+            cout << endl;
+        }
+    }
+};
+int main()
+{
+    Matrix a(5, 6);
+    a.Rand();
+    a.Print();
+    a.Min();
+    a.Find();
+    a.Submatrix();
+    return 0;
+}
